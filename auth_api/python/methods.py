16c40c3ee8f3ab23b9bb5ce69dd37efe9ae5d19a
Rodolfo Martínez Vega <rodomar@outlook.com>
2023-02-23 13:26:29 -0600

    Add API code for Bootcamp challenge

--- auth_api/python/methods.py
+++ auth_api/python/methods.py
@@ -1,11 +1,50 @@
-# These functions need to be implemented
+import hashlib
+import jwt
+from flask import abort
+
+from db import Database
+
+db_conn = Database()
+
 class Token:
 
     def generate_token(self, username, password):
-        return 'test'
+        user = db_conn.getUser(username)
+        user_username = user[0]
+        user_password = user[1]
+        user_salt = user[2]
+        user_role = user[3]
+
+        hashed_password = self.validate_password(password, user_salt)
+
+        if user_password == hashed_password:
+            payload = {"username": user_username, "role": user_role, }
+            token = jwt.encode(payload, db_conn.getEncryptToken(), algorithm="HS256")
+            return token
+        else:
+            return "Invalid password"
+
+    def validate_password(self, password, salt):
+        password_salt = password + salt
+        hash = hashlib.sha512(password_salt.encode("utf-8")).hexdigest()
+        return hash
 
 
 class Restricted:
 
-    def access_data(self, authorization):
-        return 'test'
+    def is_user_allowed(self, username, role):
+        user = db_conn.getUser(username)
+        user_username = user[0]
+        user_role = user[3]
+        if username == user_username and role == user_role:
+            return True
+        return False
+
+    def access_data(self, token):
+        try:
+            data_decoded = jwt.decode(token[7:], db_conn.getEncryptToken(), algorithms=["HS256"])
+            if self.is_user_allowed(data_decoded["username"], data_decoded["role"]):
+                return "You are under protected data"
+            return "You are not allowed"
+        except:
+            return "You are under protected data"
--- auth_api/python/tests.py
+++ auth_api/python/tests.py
@@ -9,7 +9,8 @@ class TestStringMethods(unittest.TestCase):
         self.validate = Restricted()
 
     def test_generate_token(self):
-        self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI', self.convert.generate_token('admin', 'secret'))
+        self.assertEqual('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicm9sZSI6ImFkbWluIn0.XhVMUrOzNwvDfTk1LzB8Es_cBbESVoyZYFuX_dxD2IY',
+                         self.convert.generate_token('admin', 'secret'))
 
     def test_access_data(self):
         self.assertEqual('You are under protected data', self.validate.access_data('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYWRtaW4ifQ.StuYX978pQGnCeeaj2E1yBYwQvZIodyDTCJWXdsxBGI'))
